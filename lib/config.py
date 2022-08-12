1c0cbdc4aefd84862272d6f199718a6eadb3448f
MahanBi <dirtymbgamer@gmail.com>
2022-08-12 17:32:41 +0430

    Update: lib: base view of config.py

--- lib/config.py
+++ lib/config.py
@@ -1,18 +1,21 @@
-from . import Pkg
+from . import Pkg, Xserver
 
 Main = (
     Pkg.Packages(
-            "vim",
-            "neovim",
-            "neofetch",
+        "vim",
+        "neovim",
+        "neofetch",
     ),
+    Xserver.WindowManager(
+        "i3",
+        "qtile"
+    ).x11,  # or wayland .wayland
+    Xserver.DisplayManager(
+        "sddm",
+        "emptty"
+    )
+)
 
-
-
-
-
-
-
-
+Config = (
 
 )
--- lib/wm.py
+++ lib/wm.py
@@ -4,16 +4,26 @@ class Xserver:
     def __init__(self): ...
 
     class WindowManager:
-        def __init__(self): ...
+        def __init__(self, *wm_names: str):
+            self.window_managers: tuple = wm_names
+            self._x11 = False
+            self._wayland = False
 
         def config(self, config: str):
             ...
 
-        def x11(self) -> None:
+        @property
+        def x11(self):
+            self._x11 = True
             return self
 
-        def wayland(self) -> None:
+        @property
+        def wayland(self):
+            self._wayland = True
             return self
 
     class DisplayManager:
-        def __init__(self): ...
+        def __init__(self, dm_default: str, *dm_names: str):
+            self.DM: str = dm_default
+            self.other_DM: tuple = dm_names
+
